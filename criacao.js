88114df095656404457ef8b757db593e0205bc3b
Douglas Phelipe <douglasphelipedpss@gmail.com>
2024-06-10 01:02:28 -0300

    realizado toda parte visual, faltando apenas a parte de interação.

---
+++ criacao.js
@@ -0,0 +1,43 @@
+var peca = '<i class="fa-solid fa-chess-rook"></i>';
+const gameBoard = document.querySelector(".gameBoard");
+const posicaoDasPecas = [
+    "rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook",
+    "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn",
+    " ", " ", " ", " ", " ", " ", " ", " ",
+    " ", " ", " ", " ", " ", " ", " ", " ",
+    " ", " ", " ", " ", " ", " ", " ", " ",
+    " ", " ", " ", " ", " ", " ", " ", " ",
+    "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn",
+    "rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"
+];
+
+const posicaoDosQuadradosBrancosEPretos = [
+    "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto",
+    "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco",
+    "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto",
+    "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco",
+    "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto",
+    "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco",
+    "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto",
+    "Preto", "Branco", "Preto", "Branco", "Preto", "Branco", "Preto", "Branco",
+];
+
+function criarTabuleiro() {
+    for (let i = 0; i < posicaoDasPecas.length; i++) {
+        const quadrado = document.createElement("div");
+        quadrado.setAttribute("id", i);
+        const pecaAColocar = posicaoDasPecas[i];
+
+        if (pecaAColocar != " ") {
+            quadrado.innerHTML = peca.replace("rook", posicaoDasPecas[i]);
+            if (i < 16) quadrado.firstChild.classList.add("jogador-um");
+            else if (i > 47) quadrado.firstChild.classList.add("jogador-dois");
+        }
+
+        if (posicaoDosQuadradosBrancosEPretos[i] === "Branco") quadrado.classList.add("quadrado-branco");   
+        else quadrado.classList.add("quadrado-preto");
+
+        gameBoard.appendChild(quadrado);
+    }
+}
+criarTabuleiro();
